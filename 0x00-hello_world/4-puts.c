d44c44d26c4de72582986d8ed05e38edfc59efcf
Eazyman123 <eazyman123@gmail.com>
2022-03-10 11:42:25 -0800

    last three

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Prints a string with puts
+ *
+ *   *
+ *
+ *    * Return: 0 if program executed successfully
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
